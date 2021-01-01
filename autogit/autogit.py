5a58a244ff2020c6b7ac5713ce0778a9b0f1e67b
Dibya Ghosh <dibya.ghosh@berkeley.edu>
2021-01-01 15:56:54 -0800

    Backup: 2021/01/01 15:56:54

--- autogit/autogit.py
+++ autogit/autogit.py
@@ -2,41 +2,39 @@ import os
 import os.path as osp
 import subprocess
 from datetime import datetime
-
+import functools
 
 branch_dir = osp.abspath(osp.join(osp.dirname(__file__), '..'))
 print(branch_dir)
-
-print(subprocess.run('pwd', cwd=branch_dir))
 assert osp.exists(osp.join(branch_dir, '.git')), 'Could not find .git'
 
+git_run = functools.partial(subprocess.run, cwd=branch_dir, capture_output=True)
 
-current_branch = subprocess.run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], cwd=branch_dir, capture_output=True)
+# Get current branch name with
+# git rev-parse --abbrev-ref HEAD
+current_branch = git_run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
 branch_name = current_branch.stdout.strip().decode('UTF-8')
-print(branch_dir, branch_name)
+
+# Backing up to {branch_name}-backup (e.g. master-backup) 
 backup_branch_name = f'{branch_name}-backup'
 
 # git branch --list <backupbranch>
-backup_exists = subprocess.run(['git', 'branch', '--list', backup_branch_name], cwd=branch_dir, capture_output=True)
+backup_exists = git_run(['git', 'branch', '--list', backup_branch_name])
 if len(backup_exists.stdout) == 0:
     print('Creating backup branch')
     # git branch <backupbranch>
-    backup_exists = subprocess.run(['git', 'branch', backup_branch_name], cwd=branch_dir, capture_output=True)
+    backup_exists = git_run(['git', 'branch', backup_branch_name])
 
-_ = subprocess.run(['git', 'add', '-A'], cwd=branch_dir, capture_output=True)
-print(_)
-_ = subprocess.run(['git', 'commit', '--allow-empty', '--allow-empty-message', '-m', '""'], cwd=branch_dir, capture_output=True)
-print(_)
-_ = subprocess.run(['git', 'checkout', backup_branch_name], cwd=branch_dir, capture_output=True)
-print(_)
-_ = subprocess.run(['git', 'checkout', branch_name, '.'], cwd=branch_dir, capture_output=True)
-print(_)
-_ = subprocess.run(['git', 'add', '-A'], cwd=branch_dir, capture_output=True)
-print(_)
+commands = []
+commands.append(git_run(['git', 'add', '-A']))
+commands.append(git_run(['git', 'commit', '--allow-empty', '--allow-empty-message', '-m', '""']))
+commands.append(git_run(['git', 'checkout', backup_branch_name]))
+commands.append(git_run(['git', 'checkout', branch_name, '.']))
+commands.append(git_run(['git', 'add', '-A']))
 timestamp = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-_ = subprocess.run(['git', 'commit', '-m', f'Backup: {timestamp}'], cwd=branch_dir, capture_output=True)
-print(_)
-_ = subprocess.run(['git', 'checkout', branch_name], cwd=branch_dir, capture_output=True)
-print(_)
-_ = subprocess.run(['git', 'reset', 'HEAD^'], cwd=branch_dir, capture_output=True)
-print(_)
+commands.append(git_run(['git', 'commit', '-m', f'Backup: {timestamp}']))
+commands.append(git_run(['git', 'checkout', branch_name]))
+commands.append(git_run(['git', 'reset', 'HEAD^']))
+
+for command in commands:
+    print(command)
